ea37b69e78ecd6392a36308fab832802b9b69024
Charpell <charpellumeh@gmail.com>
2017-05-17 10:47:52 -0700

    Modify findMinMax

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -31,3 +31,25 @@ module.exports.findMinMax = function(arr){
     }
     
   
+// module.exports.findMinMax = function minMax(arr) {
+//   var result = []
+//   var min = arr.reduce(function(a, b) {
+//     if (a <= b) {
+//       return a
+//     } else {
+//       return b
+//     }
+//   })
+//   var max = arr.reduce(function(a, b) {
+//     if (a >= b) {
+//       return a
+//     } else {
+//       return b
+//     }
+//   })
+//   result.push(min, max)
+//   if (result[0] === result[1]) {
+//     result = [result[0]]
+//   }
+//   return result
+// }
